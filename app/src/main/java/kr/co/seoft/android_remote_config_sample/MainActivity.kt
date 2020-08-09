daeb8ea76ac491c43b0525576a5040373e15aa26
seoft <seoft.dev@gmail.com>
2020-08-09 11:46:24 +0900

    init project & module

---
+++ app/src/main/java/kr/co/seoft/android_remote_config_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.seoft.android_remote_config_sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
